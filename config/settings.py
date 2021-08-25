982b8bcff2b74e1f5979fdb196df8e3c0a6f0df6
Ingyu Yang <79055280+igoo-Y@users.noreply.github.com>
2021-08-25 11:13:01 +0900

    users, posts apps

--- config/settings.py
+++ config/settings.py
@@ -39,7 +39,11 @@ DJANGO_APPS = [
     "django.contrib.staticfiles",
 ]
 
-PROJECT_APPS = ["core.apps.CoreConfig"]
+PROJECT_APPS = [
+    "core.apps.CoreConfig",
+    "users.apps.UsersConfig",
+    "posts.apps.PostsConfig",
+]
 
 THIRDPARTY_APPS = []
 
@@ -130,3 +134,5 @@ STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "users.User"
---
+++ posts/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+from core import models as core_models
+
+
+class Post(core_models.TimeStampedModel):
+    pass
