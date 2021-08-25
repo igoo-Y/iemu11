341d56c79d31acfd9bb3c79f623297c6a3f1e79f
Ingyu Yang <79055280+igoo-Y@users.noreply.github.com>
2021-08-25 11:30:20 +0900

    homeview, templates

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Post)
+class PostModelAdmin(admin.ModelAdmin):
+
+    """Post Model Admin Definition"""
+
+    list_display = (
+        "title",
+        "writer",
+    )
--- posts/models.py
+++ posts/models.py
@@ -1,6 +1,17 @@
 from django.db import models
+from django.db.models import base
 from core import models as core_models
+from users import models as user_models
 
 
 class Post(core_models.TimeStampedModel):
-    pass
+
+    """Post Model Definition"""
+
+    title = models.CharField(max_length=80, null=True, blank=True)
+    writer = models.ForeignKey(user_models.User, on_delete=models.CASCADE, null=True)
+    body = models.TextField(null=True, blank=True)
+    view_count = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title[:30]
