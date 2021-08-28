304e7da208661d192d708a5800547795540254ed
Ingyu Yang <79055280+igoo-Y@users.noreply.github.com>
2021-08-28 21:50:08 +0900

    intro, greeting

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 app_name = "core"
 
-urlpatterns = [path("", views.homeview, name="home")]
+urlpatterns = [
+    path("", views.home_view, name="home"),
+    path("intro/", views.intro_view, name="intro"),
+    path("greeting/", views.greeting_gview, name="greeting"),
+]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
 
-def homeview(request):
+def home_view(request):
     return render(request, "core/home.html")
+
+
+def intro_view(request):
+    return render(request, "core/intro.html")
+
+
+def greeting_gview(request):
+    return render(request, "core/greeting.html")
