330a5266d7d04caab99d8bd0d35040e000ead074
Ingyu Yang <79055280+igoo-Y@users.noreply.github.com>
2021-10-04 17:59:14 +0900

    partials - partners

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,7 @@
+from typing import List
 from django.shortcuts import render
+from django.views.generic import ListView
 
 # Create your views here.
+class PostListView(ListView):
+    template_name = "posts/post_list.html"
